7f6e4d49ed13860a71281f6257a9ead521e7f45e
pamapa <pamapa@users.noreply.github.com>
2021-10-06 14:51:17 +0200

    feat: eslint test files too

--- test/AuthProvider.test.tsx
+++ test/AuthProvider.test.tsx
@@ -1,191 +1,185 @@
-import { UserManager, User } from "oidc-client"
-import { act } from "@testing-library/react"
-import { renderHook } from "@testing-library/react-hooks"
-import { mocked } from "ts-jest/utils"
+/* eslint-disable @typescript-eslint/unbound-method */
+import { UserManager, User } from "oidc-client";
+import { renderHook } from "@testing-library/react-hooks";
+import { mocked } from "ts-jest/utils";
 
-import { useAuth } from "../src/useAuth"
-import { createWrapper } from "./helpers"
+import { useAuth } from "../src/useAuth";
+import { createWrapper } from "./helpers";
 
-const user = { id_token: "__test_user__" } as User
+const user = { id_token: "__test_user__" } as User;
 
 describe("AuthProvider", () => {
     it("should signinRedirect when asked", async () => {
         // arrange
-        const wrapper = createWrapper()
+        const wrapper = createWrapper();
         const { waitForNextUpdate, result } = renderHook(() => useAuth(), {
             wrapper,
-        })
-        await waitForNextUpdate()
-        expect(result.current.user).toBeUndefined()
+        });
+        await waitForNextUpdate();
+        expect(result.current.user).toBeUndefined();
 
         // act
-        await act(async () => {
-            result.current.signinRedirect()
-        })
+        await result.current.signinRedirect();
 
         // assert
-        expect(UserManager.prototype.signinRedirect).toHaveBeenCalled()
-        expect(UserManager.prototype.getUser).toHaveBeenCalled()
-    })
+        expect(UserManager.prototype.signinRedirect).toHaveBeenCalled();
+        expect(UserManager.prototype.getUser).toHaveBeenCalled();
+    });
 
     it("should handle signinCallback success and call onSigninCallback", async () => {
         // arrange
-        const onSigninCallback = jest.fn()
+        const onSigninCallback = jest.fn();
         window.history.pushState(
             {},
             document.title,
             "/?code=__test_code__&state=__test_state__"
-        )
+        );
         expect(window.location.href).toBe(
             "https://www.example.com/?code=__test_code__&state=__test_state__"
-        )
+        );
 
-        const wrapper = createWrapper({ onSigninCallback })
+        const wrapper = createWrapper({ onSigninCallback });
 
         // act
         const { waitForNextUpdate } = renderHook(() => useAuth(), {
             wrapper,
-        })
-        await waitForNextUpdate()
+        });
+        await waitForNextUpdate();
 
         // assert
-        expect(UserManager.prototype.signinCallback).toHaveBeenCalled()
-        expect(onSigninCallback).toHaveBeenCalled()
-    })
+        expect(UserManager.prototype.signinCallback).toHaveBeenCalled();
+        expect(onSigninCallback).toHaveBeenCalled();
+    });
 
     it("should handle signinCallback errors and call onSigninCallback", async () => {
         // arrange
-        const onSigninCallback = jest.fn()
+        const onSigninCallback = jest.fn();
         window.history.pushState(
             {},
             document.title,
             "/?error=__test_error__&state=__test_state__"
-        )
+        );
         expect(window.location.href).toBe(
             "https://www.example.com/?error=__test_error__&state=__test_state__"
-        )
+        );
 
-        const wrapper = createWrapper({ onSigninCallback })
+        const wrapper = createWrapper({ onSigninCallback });
 
         // act
         const { waitForNextUpdate } = renderHook(() => useAuth(), {
             wrapper,
-        })
-        await waitForNextUpdate()
+        });
+        await waitForNextUpdate();
 
         // assert
-        expect(UserManager.prototype.signinCallback).toHaveBeenCalled()
-        expect(onSigninCallback).toHaveBeenCalled()
-    })
+        expect(UserManager.prototype.signinCallback).toHaveBeenCalled();
+        expect(onSigninCallback).toHaveBeenCalled();
+    });
 
     it("should handle removeUser and call onRemoveUser", async () => {
         // arrange
-        const onRemoveUser = jest.fn()
+        const onRemoveUser = jest.fn();
 
-        const wrapper = createWrapper({ onRemoveUser })
+        const wrapper = createWrapper({ onRemoveUser });
         const { waitForNextUpdate, result } = renderHook(() => useAuth(), {
             wrapper,
-        })
-        await waitForNextUpdate()
+        });
+        await waitForNextUpdate();
 
         // act
-        await act(async () => {
-            result.current.removeUser()
-        })
+        await result.current.removeUser();
 
         // assert
-        expect(UserManager.prototype.removeUser).toHaveBeenCalled()
-        expect(onRemoveUser).toHaveBeenCalled()
-    })
+        expect(UserManager.prototype.removeUser).toHaveBeenCalled();
+        expect(onRemoveUser).toHaveBeenCalled();
+    });
 
     it("should handle signoutRedirect and call onSignoutRedirect", async () => {
         // arrange
-        const onSignoutRedirect = jest.fn()
-        const wrapper = createWrapper({ onSignoutRedirect })
+        const onSignoutRedirect = jest.fn();
+        const wrapper = createWrapper({ onSignoutRedirect });
         const { waitForNextUpdate, result } = renderHook(() => useAuth(), {
             wrapper,
-        })
-        await waitForNextUpdate()
+        });
+        await waitForNextUpdate();
 
         // act
-        await act(async () => {
-            result.current.signoutRedirect()
-        })
+        await result.current.signoutRedirect();
 
         // assert
-        expect(UserManager.prototype.signoutRedirect).toHaveBeenCalled()
-        expect(onSignoutRedirect).toHaveBeenCalled()
-    })
+        expect(UserManager.prototype.signoutRedirect).toHaveBeenCalled();
+        expect(onSignoutRedirect).toHaveBeenCalled();
+    });
 
     it("should handle signoutPopup and call onSignoutPopup", async () => {
         // arrange
-        const onSignoutPopup = jest.fn()
-        const wrapper = createWrapper({ onSignoutPopup })
+        const onSignoutPopup = jest.fn();
+        const wrapper = createWrapper({ onSignoutPopup });
         const { waitForNextUpdate, result } = renderHook(() => useAuth(), {
             wrapper,
-        })
-        await waitForNextUpdate()
+        });
+        await waitForNextUpdate();
 
         // act
-        await act(async () => {
-            result.current.signoutPopup()
-        })
+        await result.current.signoutPopup();
 
         // assert
-        expect(UserManager.prototype.signoutPopup).toHaveBeenCalled()
-        expect(onSignoutPopup).toHaveBeenCalled()
-    })
+        expect(UserManager.prototype.signoutPopup).toHaveBeenCalled();
+        expect(onSignoutPopup).toHaveBeenCalled();
+    });
 
     it("should get the user", async () => {
         // arrange
-        mocked(UserManager.prototype).getUser.mockResolvedValueOnce(user)
-        const wrapper = createWrapper()
+        mocked(UserManager.prototype).getUser.mockResolvedValueOnce(user);
+        const wrapper = createWrapper();
 
         // act
         const { waitForNextUpdate, result } = renderHook(() => useAuth(), {
             wrapper,
-        })
-        await waitForNextUpdate()
+        });
+        await waitForNextUpdate();
 
         // assert
-        expect(result.current.user).toBe(user)
-    })
+        expect(result.current.user).toBe(user);
+    });
 
     it("should use a custom UserManager implementation", async () => {
         // arrange
         class CustomUserManager extends UserManager { }
         mocked(CustomUserManager.prototype).signinRedirect = jest.fn();
 
-        const wrapper = createWrapper({ implementation: CustomUserManager })
+        const wrapper = createWrapper({ implementation: CustomUserManager });
         const { waitForNextUpdate, result } = renderHook(() => useAuth(), {
             wrapper,
-        })
-        await waitForNextUpdate()
-        expect(result.current.user).toBeUndefined()
+        });
+        await waitForNextUpdate();
+        expect(result.current.user).toBeUndefined();
 
         // act
-        await act(async () => {
-            result.current.signinRedirect()
-        })
+        await result.current.signinRedirect();
 
         // assert
-        expect(UserManager.prototype.signinRedirect).not.toHaveBeenCalled()
-        expect(CustomUserManager.prototype.signinRedirect).toHaveBeenCalled()
-    })
+        expect(UserManager.prototype.signinRedirect).not.toHaveBeenCalled();
+        expect(CustomUserManager.prototype.signinRedirect).toHaveBeenCalled();
+    });
 
     it("should should throw when no UserManager implementation exists", async () => {
         // arrange
-        const wrapper = createWrapper({ implementation: null })
+        const wrapper = createWrapper({ implementation: null });
         const { result } = renderHook(() => useAuth(), {
             wrapper,
-        })
+        });
 
         // act
-        await expect(act(async () => {
-            result.current.signinRedirect()
-        })).rejects.toThrow()
+        try {
+            await result.current.signinRedirect();
+            fail("should not come here");
+        }
+        catch (err) {
+            expect(err).toBeInstanceOf(Error);
+        }
 
         // assert
-        expect(UserManager.prototype.signinRedirect).not.toHaveBeenCalled()
-    })
-})
+        expect(UserManager.prototype.signinRedirect).not.toHaveBeenCalled();
+    });
+});
