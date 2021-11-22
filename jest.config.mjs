1fcbd4f99cc32d94bb4e57c7185a661c90cae1bb
pamapa <pamapa@users.noreply.github.com>
2021-11-22 12:27:15 +0100

    build: add codecov uploader job

--- jest.config.mjs
+++ jest.config.mjs
@@ -1,9 +1,15 @@
-module.exports = {
+import yn from "yn";
+
+const collectCoverage = yn(process.env.CI);
+
+export default {
     preset: "ts-jest",
     clearMocks: true,
     testMatch: ["**/test/**/*.test.tsx"],
     testEnvironment: "jsdom",
     testURL: "https://www.example.com/",
+    collectCoverage,
+    coverageReporters: collectCoverage ? ["lcov"] : ["lcov", "text"],
     globals: {
         "ts-jest": {
             // skip ts-jest type checking, incremental compilation with tsc is much faster
