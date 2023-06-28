07dda282199b1e642c0a7d3b345603a6303ea3a0
Chris Lacaille <ChrisL108@users.noreply.github.com>
2023-06-28 10:18:12 -0500

    feat: Dispatch errors to state in AuthProvider (#820)
    * feat: Dispatch errors to state in AuthProvider
    
    - Errors during authentication navigation methods are now caught and dispatched to the state for better error handling and user feedback.

--- src/AuthProvider.tsx
+++ src/AuthProvider.tsx
@@ -187,6 +187,9 @@ export const AuthProvider = (props: AuthProviderProps): JSX.Element => {
                                 });
                                 try {
                                     return await userManager[key](...args);
+                                } catch (error) {
+                                    dispatch({ type: "ERROR", error: error as Error });
+                                    return null;
                                 } finally {
                                     dispatch({ type: "NAVIGATOR_CLOSE" });
                                 }
