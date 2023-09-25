90560bbb264fcebc2166e1c85e9942650fbdbf44
Giovanne Tarcitano <giovannetarcitano@gmail.com>
2023-09-25 12:08:29 -0300

    fix(build): generate esm package.json with version (#936)
    * fix(build): generate esm package.json with version

--- scripts/build.js
+++ scripts/build.js
@@ -1,8 +1,9 @@
 #!/usr/bin/env node
 const { buildSync } = require("esbuild");
 const { join } = require("path");
+const fs = require("fs");
 
-const { dependencies, peerDependencies } = require("../package.json");
+const { dependencies, peerDependencies, version } = require("../package.json");
 
 const opts = {
     entryPoints: ["src/index.ts"],
@@ -25,6 +26,9 @@ try {
         platform: "node",
         outfile: "dist/umd/react-oidc-context.js",
     });
+    // generate package.json for esm
+    const distPackageJson = { type: "module" , version };
+    fs.writeFileSync("dist/esm/package.json", JSON.stringify(distPackageJson, null, 2) + "\n");
 } catch (err) {
     // esbuild handles error reporting
     process.exitCode = 1;
