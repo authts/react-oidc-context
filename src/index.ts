70e5c53e92d799ee8abe3c502371f7bd44692348
aBytex <andreas@hribars.de>
2025-01-25 17:44:33 +0100

    ✨: Add export of useAutoSignin for build

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ export * from "./AuthContext";
 export * from "./AuthProvider";
 export type { AuthState } from "./AuthState";
 export * from "./useAuth";
+export * from "./useAutoSignin";
 export { hasAuthParams } from "./utils";
 export * from "./withAuth";
 export * from "./withAuthenticationRequired";
